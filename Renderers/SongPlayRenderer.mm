bc2b32705631e4a950f1d82f98db3600a836ee43
Alex Kremer <godexsoft@gmail.com>
2010-06-22 09:28:21 +0000

    autoplay - off. new credits

--- Renderers/SongPlayRenderer.mm
+++ Renderers/SongPlayRenderer.mm
@@ -154,7 +154,7 @@ extern TMGameState* g_pGameState;
 	[g_pGameState->m_pSteps dump];
 #endif	
 	
-	g_pGameState->m_bAutoPlay = YES;
+	g_pGameState->m_bAutoPlay = NO;
 	g_pGameState->m_nFailType = kFailAtEnd;
 	g_pGameState->m_nCombo = 0;
 	
