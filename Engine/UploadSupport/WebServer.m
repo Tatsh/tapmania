a515a6cf744b55242a43ce2abb604ce380bfc25c
Alex Kremer <godexsoft@gmail.com>
2010-09-02 22:04:49 +0000

    en0 is the wifi, not en1. changed back before i forget

--- Engine/UploadSupport/WebServer.m
+++ Engine/UploadSupport/WebServer.m
@@ -520,7 +520,7 @@ answer_to_connection (void *cls, struct MHD_Connection *connection,
 		while(temp_addr != NULL)  { 
 			if(temp_addr->ifa_addr->sa_family == AF_INET)  {
 				// Check if interface is en0 which is the wifi connection on the iPhone
-				if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en1"])  { 
+				if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"])  { 
 					// Get NSString from C String 
 					address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)]; 
 					address = [NSString stringWithFormat:@"http://%@:9002/", address];
